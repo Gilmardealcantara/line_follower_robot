0601e918f61cbf744fd20fde27b609c831a56d11
gilmardealcantara <gilmardealcantara@gmail.com>
2017-01-05 15:41:47 -0200

    Add program to test engines.

---
+++ test_motor/test_motor.ino
@@ -0,0 +1,20 @@
+#include <AFMotor.h> // https://learn.adafruit.com/adafruit-motor-shield/library-install
+
+AF_DCMotor motorEsq(1, MOTOR12_64KHZ);
+AF_DCMotor motorDir(2, MOTOR12_64KHZ);
+
+void setup() {
+  motorEsq.setSpeed(0);
+  motorDir.setSpeed(0);
+  motorEsq.run(RELEASE);
+  motorDir.run(RELEASE);
+  Serial.begin(9600);
+}
+
+void loop() {
+ motorDir.setSpeed(255);
+ motorEsq.setSpeed(255); 
+ motorDir.run(FORWARD);
+ motorEsq.run(FORWARD);
+
+}
